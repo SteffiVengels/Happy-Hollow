b843ed08be7a1ab9cd401bacf0df186dcd4b38bf
Steffi Vengels <vengels.stefanie@gmail.com>
2025-06-26 19:40:55 +0200

    finished helthbar

--- classes/status-bar.class.js
+++ classes/status-bar.class.js
@@ -1,10 +1,13 @@
 class StatusBar extends DrawableObject {
     IMAGES = [
-        './assets/img/game-ui-pixel-art/2 Bars/Healthbar_full.png',
-        './assets/img/game-ui-pixel-art/2 Bars/Healthbar_full.png',
-        './assets/img/game-ui-pixel-art/2 Bars/Healthbar_full.png',
-        './assets/img/game-ui-pixel-art/2 Bars/Healthbar_full.png',
-        './assets/img/game-ui-pixel-art/2 Bars/Healthbar_full.png',
+        './assets/img/game-ui-pixel-art/2 Bars/Healthbar_empty.png',
+        './assets/img/game-ui-pixel-art/2 Bars/healthbar/healthbar12.5.png',
+        './assets/img/game-ui-pixel-art/2 Bars/healthbar/healthbar25.png',
+        './assets/img/game-ui-pixel-art/2 Bars/healthbar/healthbar37.5.png',
+        './assets/img/game-ui-pixel-art/2 Bars/healthbar/healthbar50.png',
+        './assets/img/game-ui-pixel-art/2 Bars/healthbar/healthbar62.5.png',
+        './assets/img/game-ui-pixel-art/2 Bars/healthbar/healthbar75.png',
+        './assets/img/game-ui-pixel-art/2 Bars/healthbar/healthbar87.5.png',
         './assets/img/game-ui-pixel-art/2 Bars/Healthbar_full.png'
     ];
     percentage = 100;
@@ -26,18 +29,24 @@ class StatusBar extends DrawableObject {
     }
 
     resolveImageIndex() {
-        if (this.percentage == 100) {
-            return 5;
-        } else if (this.percentage > 80) {
-            return 4;
-        } else if (this.percentage > 60) {
-            return 3;
-        } else if (this.percentage > 40) {
-            return 2;
-        } else if (this.percentage > 20) {
-            return 1;
-        } else {
+        if (this.percentage == 0) {
             return 0;
+        } else if (this.percentage < 12.5) {
+            return 1;
+        } else if (this.percentage < 25) {
+            return 2;
+        } else if (this.percentage < 37.5) {
+            return 3;
+        } else if (this.percentage < 50) {
+            return 4;
+        } else if (this.percentage < 62.5) {
+            return 5;
+        } else if (this.percentage < 75) {
+            return 6;
+        } else if (this.percentage < 87.5) {
+            return 7;
+        } else {
+            return 8;
         }
     }
 }
