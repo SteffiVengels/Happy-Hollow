b6b81da7e3693c981f4a1258b1d3928c377df60b
Steffi Vengels <vengels.stefanie@gmail.com>
2025-08-10 17:33:58 +0200

    clean code for audoManager class

--- classes/audioManager.class.js
+++ classes/audioManager.class.js
@@ -49,6 +49,10 @@ class AudioManager {
     }
 
 
+    /**
+     * Sets the volume for all sounds.
+     * @param {number} volume - Volume level between 0.0 and 1.0.
+     */
     setGlobalVolume(volume) {
         this.allSounds.forEach(audio => {
             if (audio) {
@@ -57,6 +61,11 @@ class AudioManager {
         });
     }
 
+
+    /**
+     * Plays a given sound effect if sound is enabled.
+     * @param {HTMLAudioElement} sound - The sound to play.
+     */
     playSound(sound) {
         if (soundOn) {
             sound.currentTime = 0;
@@ -65,6 +74,11 @@ class AudioManager {
     }
 
 
+    /**
+     * Plays background music if music is enabled.
+     * Loops the track automatically.
+     * @param {HTMLAudioElement} music - The music to play.
+     */
     playMusic(music) {
         if (musicOn) {
             music.currentTime = 0;
@@ -74,11 +88,16 @@ class AudioManager {
     }
 
 
+    /**
+     * Stops a playing music track.
+     * @param {HTMLAudioElement} music - The music to stop.
+     */
     stopMusic(music) {
         music.pause();
     }
 
 
+    /** Stops all background music and plays the death sound. */
     stopBackgroundMusicAndPlayDeathSound() {
         this.stopLevel1BackgroundMusic();
         this.stopEndbossLevel1BackgroundMusic();
@@ -86,102 +105,122 @@ class AudioManager {
     }
 
 
+    /** Stops menu music and plays the game start sound. */
     stopMenuMusicAndPlayGameStartSound() {
         this.stopMenuMusic();
         this.playSound(this.AUDIO_GAME_START);
     }
 
 
+    /** Stops the menu music. */
     stopMenuMusic() {
         this.stopMusic(this.AUDIO_MENU);
     }
 
 
+    /** Stops Level 1 background music. */
     stopLevel1BackgroundMusic() {
         this.stopMusic(this.AUDIO_LEVEL1_BACKGROUND);
     }
 
 
+    /** Stops Endboss Level 1 background music. */
     stopEndbossLevel1BackgroundMusic() {
         this.stopMusic(this.AUDIO_ENDBOSSLEVEL1_BACKGROUND);
     }
 
 
+    /** Plays the coin collection sound. */
     playCollectCoinSound() {
         this.playSound(this.AUDIO_COLLECT_COINS);
     }
 
 
+    /** Plays the jumping sound. */
     playJumpingSound() {
         this.playSound(this.AUDIO_JUMPING);
     }
 
 
+    /** Plays the throw sound. */
     playThrowSound() {
         this.playSound(this.AUDIO_THROW);
     }
 
 
+    /** Plays the hurt sound. */
     playHurtSound() {
         this.playSound(this.AUDIO_HURT);
     }
 
 
+    /** Plays the enemy attack sound. */
     playEnemieAttackSound() {
         this.playSound(this.AUDIO_ENEMIE_ATTACK);
     }
 
 
+    /** Plays the enemy dead sound. */
     playEnemieDeadSound() {
         this.playSound(this.AUDIO_ENEMIE_DEAD);
     }
 
 
+    /** Plays the endboss sneer sound. */
     playEndbossSneerSound() {
         this.playSound(this.AUDIO_ENDBOSS_SNEER);
     }
 
 
+    /** Plays the endboss attack sound. */
     playEndbossAttackSound() {
         this.playSound(this.AUDIO_ENDBOSS_ATTACK);
     }
 
 
+    /** Plays the endboss dead sound. */
     playEndbossDeadSound() {
         this.playSound(this.AUDIO_ENDBOSS_DEAD);
     }
 
 
+    /** Plays the food collection sound. */
     playCollectFoodSound() {
         this.playSound(this.AUDIO_COLLECT_FOOD);
     }
 
 
+    /** Plays the game over sound. */
     playGameOverSound() {
         this.playSound(this.AUDIO_GAME_OVER);
     }
 
 
+    /** Plays the game win sound. */
     playGameWinSound() {
         this.playSound(this.AUDIO_GAME_WIN);
     }
 
 
+    /** Plays the button click sound. */
     playButtonSound() {
         this.playSound(this.AUDIO_BUTTON);
     }
 
 
+    /** Plays the menu music. */
     playMenuMusic() {
         this.playMusic(this.AUDIO_MENU);
     }
 
-
+    
+    /** Plays Level 1 background music. */
     playLevel1BackgroundMusic() {
         this.playMusic(this.AUDIO_LEVEL1_BACKGROUND);
     }
 
 
+    /** Plays Endboss Level 1 background music. */
     playEndbossLevel1BackgroundMusic() {
         this.playMusic(this.AUDIO_ENDBOSSLEVEL1_BACKGROUND);
     }
